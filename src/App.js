75861dd733cab9820efe0790f6d4d2af9f772568
marizoo <marizoo@meow.woof>
2021-10-31 16:52:09 +0700

    added show page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Show from './pages/Show';
 import Starred from './pages/Starred';
 
 export default function App() {
@@ -14,7 +15,13 @@ export default function App() {
         <Starred />
       </Route>
 
-      <Route>This is 404 page</Route>
+      <Route exact path="/show/:id">
+        <Show />
+      </Route>
+
+      <Route>
+        <div>This is 404 page</div>
+      </Route>
     </Switch>
   );
 }
---
+++ src/pages/Show.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { apiGet } from '../misc/config';
+
+function Show() {
+  const { id } = useParams();
+
+  const [show, setShow] = useState(null);
+
+  useEffect(() => {
+    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results => {
+      setShow(results);
+    });
+  }, [id]);
+
+  console.log('show', show);
+
+  return (
+    <div>
+      <h2>THis is SHOW PAGE</h2>
+    </div>
+  );
+}
+
+export default Show;
